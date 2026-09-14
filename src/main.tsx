5b1d1d3bb6788b62362c422397b6f46838cd5c1d
zydan25 <zydanalattab25w@gmail.com>
2026-09-14 18:46:54 +0300

    feat: bootstrap first-start flask migration

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { fetchCurrentUser } from './api';
+import { runFirstStartFlaskMigration, registerFlaskMigrationListener } from './migration/flaskSeed';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
+    </React.StrictMode>,
   );
 }
+
+registerFlaskMigrationListener();
+void fetchCurrentUser().then((user) => runFirstStartFlaskMigration(user));
